df47a74e3a532b9df39fcfe6f0894028924de76c
Machahary07 <jeumachahary07@gmail.com>
2025-04-05 12:44:46 +0530

    frontendfiles

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -11,10 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.ATLAS_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.ATLAS_URI)
 .then(() => console.log('✅ MongoDB connected'))
 .catch((err) => console.error('❌ MongoDB connection error:', err));
 
